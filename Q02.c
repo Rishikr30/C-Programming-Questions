2b0e0605864bac270bdc487ffa6b70d17d360287
Rishi Kumar <rishikr30@outlook.com>
2025-01-28 23:59:59 +0530

    Write a programme sum two numbers.

---
+++ Q02.c
@@ -0,0 +1,10 @@
+//Write a programme sum two numbers.
+
+#include <stdio.h>
+
+int main() {
+    int a = 5, b = 10;
+    int sum = a + b;
+    printf("Sum of %d and %d: %d\n",a,b,sum);
+    return 0;
+}
