303baa9a02ca1bc33c60defba20ee256f37f49c2
Rishi Kumar <rishikr30@outlook.com>
2025-01-29 00:40:10 +0530

    W.a.p to sum of 10 natural numbers.

---
+++ Q21.c
@@ -0,0 +1,13 @@
+//W.a.p to sum of 10 natural numbers.
+
+
+#include <stdio.h>
+
+int main() {
+    int sum = 0;
+    for (int i = 1; i <= 10; i++) {
+        sum += i;
+    }
+    printf("Sum: %d\n", sum);
+    return 0;
+}
