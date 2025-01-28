ac4d80f798bee349c085e42ee27a129e044fff22
Rishi Kumar <rishikr30@outlook.com>
2025-01-29 01:00:55 +0530

    W.a.p to print pattern.

---
+++ Q33(4).c
@@ -0,0 +1,13 @@
+//w.a.p. to print the following pattern
+
+#include <stdio.h>
+
+int main() {
+    for (int i = 5; i >= 1; i--) {
+        for (int j = i; j >= 1; j--) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
