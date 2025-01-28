ad8769f3ee8e559f0bea3ade5e43299d05aaf1aa
Rishi Kumar <rishikr30@outlook.com>
2025-01-29 00:11:32 +0530

    W.a.p to enter two numbers and swap them.

---
+++ Q07.c
@@ -0,0 +1,16 @@
+//Write a program to enter two numbers from keyboard and swap them.
+
+
+
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    temp = a;
+    a = b;
+    b = temp;
+    printf("After swapping: a = %d, b = %d\n", a, b);
+    return 0;
+}
