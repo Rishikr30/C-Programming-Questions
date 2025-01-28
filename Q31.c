4864ad8895b91e27d7202cb26de37fec3d1389cc
Rishi Kumar <rishikr30@outlook.com>
2025-01-29 00:53:01 +0530

    W.a.p to print all prime numbers b/w two range.

---
+++ Q31.c
@@ -0,0 +1,20 @@
+//W.a.p to print all prime numbers between two given range.
+
+
+#include <stdio.h>
+
+int is_prime(int n) {
+    if (n < 2) return 0;
+    for (int i = 2; i * i <= n; i++)
+        if (n % i == 0) return 0;
+    return 1;
+}
+
+int main() {
+    int start, end;
+    printf("Enter range: ");
+    scanf("%d %d", &start, &end);
+    for (int i = start; i <= end; i++)
+        if (is_prime(i)) printf("%d ", i);
+    return 0;
+}
