f0ec201a80cd6c5d6a4e00e3bcdfdf8f12abb00d
Rishi Kumar <rishikr30@outlook.com>
2025-01-29 01:29:24 +0530

    W.a.p to create a function to print your name.

---
+++ Q52.c
@@ -0,0 +1,12 @@
+//W.a.p to create a function to print your name.
+
+#include <stdio.h>
+
+void printName() {
+    printf("Your name is Rishi Kumar.\n");
+}
+
+int main() {
+    printName();
+    return 0;
+}
