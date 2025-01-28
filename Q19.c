f7ac4445cc82c44bccd2fcb83e578ddaa9148b61
Rishi Kumar <rishikr30@outlook.com>
2025-01-29 00:37:53 +0530

    W.a.p to print your name 10 times using loop.

---
+++ Q19.c
@@ -0,0 +1,12 @@
+// W.a.p to print your name 10 times using loop statement.
+
+
+
+#include <stdio.h>
+
+int main() {
+    for (int i = 0; i < 10; i++) {
+        printf("Rishi Kumar\n");
+    }
+    return 0;
+}
